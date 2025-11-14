16695ddcbc9b4b0873fe5bf7b01b57a35e15bc8a
Matvey Fedorov <mfgolden@yandex.ru>
2025-11-14 16:59:23 +0300

    fix 2

--- services/user-service/internal/controller/http/v1/user.go
+++ services/user-service/internal/controller/http/v1/user.go
@@ -35,11 +35,15 @@ func newUserRoutes(r chi.Router, userService service.User) {
 }
 
 func writeError(w http.ResponseWriter, status int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(status)
 	_ = json.NewEncoder(w).Encode(map[string]string{"error": msg})
 }
 
 func writeJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(status)
 	_ = json.NewEncoder(w).Encode(data)
 }
