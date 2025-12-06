7cc00698cfc2c61d5d3fed04da0042dd110afa45
Matvey Fedorov <mfgolden@yandex.ru>
2025-12-06 16:20:38 +0300

    feat(users): decrypter interface and realisation

---
+++ services/user-service/internal/repo/crypto/encrypter.go
@@ -0,0 +1,77 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+type Decrypter interface {
+	Encrypt(plainText string) (cipherText string, err error)
+	Decrypt(cipherText string) (plainText string, err error)
+}
+
+type AESCryptoService struct {
+	key []byte
+}
+
+func NewAESCryptoService(key []byte) *AESCryptoService {
+	return &AESCryptoService{
+		key: key,
+	}
+}
+
+func (s *AESCryptoService) Encrypt(plainText string) (string, error) {
+	block, err := aes.NewCipher(s.key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	cipherText := gcm.Seal(nonce, nonce, []byte(plainText), nil)
+
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+func (s *AESCryptoService) Decrypt(cipherText string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode base64: %w", err)
+	}
+
+	block, err := aes.NewCipher(s.key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return "", fmt.Errorf("ciphertext too short")
+	}
+
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+
+	plainText, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainText), nil
+}
